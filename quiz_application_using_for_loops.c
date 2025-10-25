2c1b5b069e2c15df44b3cf28f85f877301d6ee3e
ayushdwivedi0307-wq <ayush.dwivedi0307@gmail.com>
2025-10-25 00:52:39 -0700

    Add files via upload

---
+++ quiz_application_using_for_loops.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<string.h>
+//Maths quiz
+struct questions{
+    int answer;
+    char ques[600];
+};
+
+int main() {
+    int user_input[5];
+    struct questions q[5];
+    strcpy(q[0].ques,"What Is The Sum Of 1 and 67\n");
+    q[0].answer = 68;
+    strcpy(q[1].ques,"What Is Answer If we divide 9 by 3\n");
+    q[1].answer = 3;
+    strcpy(q[2].ques,"What Is Answer If We Mutiply 6 with 7\n");
+    q[2].answer = 42;
+    strcpy(q[3].ques,"What Is Answer If We square the number 4\n");
+    q[3].answer = 16;
+    strcpy(q[4].ques,"What Is Answer If We Take Square Root 36\n");
+    q[4].answer = 6;
+
+    printf("Give The answers of following questions\n");
+    printf("Question Number 1: %s\n",q[0].ques);
+    printf("Question Number 2: %s\n",q[1].ques);
+    printf("Question Number 3: %s\n",q[2].ques);
+    printf("Question Number 4: %s\n",q[3].ques);
+    printf("Question Number 5: %s\n",q[4].ques);
+
+    for(int i=0;i<5;i++){
+        scanf("%d",&user_input[i]);
+        if(user_input[i]==q[i].answer){
+            printf("Your Answer Is Correct");
+        }
+        else{
+            printf("Wrong Answer");
+        }
+    }
+    return 0;
+}   // scanf me \n use nahi krna hota iska matlab next line nahi hota hai
